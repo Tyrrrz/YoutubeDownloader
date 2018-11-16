d7ca0b81a0537ac570770da87ab0cb1c233c8d17
Alexey Golub <Tyrrrrrr@gmail.com>
2018-11-16 21:59:42 +0200

    Increase connection limit

--- YoutubeDownloader/App.xaml.cs
+++ YoutubeDownloader/App.xaml.cs
@@ -1,10 +1,17 @@
-﻿using System.Windows;
+﻿using System.Net;
+using System.Windows;
 using System.Windows.Threading;
 
 namespace YoutubeDownloader
 {
     public partial class App
     {
+        static App()
+        {
+            // Increase connection limit to allow multiple simultaneous downloads
+            ServicePointManager.DefaultConnectionLimit = 10;
+        }
+
         private void App_OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs args)
         {
             MessageBox.Show(args.Exception.ToString(), "Error occured", MessageBoxButton.OK, MessageBoxImage.Error);
