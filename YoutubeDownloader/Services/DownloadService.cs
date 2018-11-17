9242139bd3efae400eb4aec4be9c761a16ca82df
Alexey Golub <Tyrrrrrr@gmail.com>
2018-11-17 22:39:00 +0200

    Small cleanup

--- YoutubeDownloader/Services/DownloadService.cs
+++ YoutubeDownloader/Services/DownloadService.cs
@@ -7,10 +7,10 @@ namespace YoutubeDownloader.Services
 {
     public class DownloadService
     {
-        private readonly IYoutubeConverter _converter = new YoutubeConverter();
+        private readonly IYoutubeConverter _youtubeConverter = new YoutubeConverter();
 
         public Task DownloadVideoAsync(string videoId, string filePath,
             IProgress<double> progress, CancellationToken cancellationToken) =>
-            _converter.DownloadVideoAsync(videoId, filePath, progress, cancellationToken);
+            _youtubeConverter.DownloadVideoAsync(videoId, filePath, progress, cancellationToken);
     }
 }
