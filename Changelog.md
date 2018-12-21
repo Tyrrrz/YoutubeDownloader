f6da340b6cc2d1e75c811a3cb1fe4b3d0c480205
Oleksii Holub <Oleksii.Holub@3shape.com>
2018-12-21 17:41:41 +0200

    Update version

---
+++ Changelog.md
@@ -0,0 +1,7 @@
+### v1.1 (21-Dec-2018)
+
+- Improved UI in all screens.
+- Limited the number of concurrent downloads and added an option in settings to configure it.
+- Last used download format is now persisted so you don't have to select it all the time.
+- Fixed an issue where temporary files were not deleted after the download was canceled. The underlying issue was fixed in YoutubeExplode.Converter v1.0.3 and CliWrap v2.2.
+- Fixed an issue where starting multiple downloads would cause them to be added in the wrong order to the list of downloads.
--- YoutubeDownloader/Properties/AssemblyInfo.cs
+++ YoutubeDownloader/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("YoutubeDownloader")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright © 2018 Alexey Golub")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -49,5 +49,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
