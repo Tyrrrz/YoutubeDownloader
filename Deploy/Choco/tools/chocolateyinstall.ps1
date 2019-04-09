e60cd690c252e0b6075bb04e302b7663c84ca226
Oleksii Holub <Oleksii.Holub@3shape.com>
2019-04-09 18:16:15 +0300

    Fix chocolatey package

--- Deploy/Choco/tools/chocolateyinstall.ps1
+++ Deploy/Choco/tools/chocolateyinstall.ps1
@@ -1,15 +1,17 @@
-﻿$ErrorActionPreference = 'Stop';
+﻿$ErrorActionPreference = 'Stop'
+$packageName = $env:ChocolateyPackageName
+$installDirPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 # Install package
 $packageArgs = @{
-    packageName   = $env:ChocolateyPackageName
-    unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+    packageName   = $packageName
+    unzipLocation = $installDirPath
     url           = 'https://github.com/Tyrrrz/YoutubeDownloader/releases/download/1.3.1/YoutubeDownloader.zip'
 }
 Install-ChocolateyZipPackage @packageArgs
 
 # Mark the executable as GUI
-New-Item (Join-Path unzipLocation "YoutubeDownloader.exe.gui") -ItemType File -Force
+New-Item (Join-Path $installDirPath "YoutubeDownloader.exe.gui") -ItemType File -Force
 
 # Don't put ffmpeg.exe on PATH
-New-Item (Join-Path unzipLocation "ffmpeg.exe.ignore") -ItemType File -Force
+New-Item (Join-Path $installDirPath "ffmpeg.exe.ignore") -ItemType File -Force
