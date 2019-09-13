4788366c8c7586551ae509e2fb8da59623cca122
Alexey Golub <Tyrrrrrr@gmail.com>
2019-09-13 21:27:40 +0300

    Add new line to query text box only on shift+enter

--- YoutubeDownloader/Views/RootView.xaml.cs
+++ YoutubeDownloader/Views/RootView.xaml.cs
@@ -1,12 +1,24 @@
-﻿using System.Windows;
+﻿using System.Windows.Input;
 
 namespace YoutubeDownloader.Views
 {
-    public partial class RootView : Window
+    public partial class RootView
     {
         public RootView()
         {
             InitializeComponent();
+        }
+
+        private void QueryTextBox_OnPreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            // Disable new lines when pressing enter without shift
+            if (e.Key == Key.Enter && e.KeyboardDevice.Modifiers != ModifierKeys.Shift)
+            {
+                e.Handled = true;
+
+                // We handle the event here so we have to directly "press" the default button
+                AccessKeyManager.ProcessKey(null, "\x000D", false);
+            }
         }
     }
 }
