1949a536c7390c80d00ecc93ffec3032e0fc0589
Moustafa Khalil <mostafa90@gmail.com>
2019-11-11 22:29:37 +0400

    Remove White Spaces

--- YoutubeDownloader/ViewModels/Dialogs/DownloadSingleSetupViewModel.cs
+++ YoutubeDownloader/ViewModels/Dialogs/DownloadSingleSetupViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Windows;
 using Tyrrrz.Extensions;
@@ -11,65 +12,68 @@ using YoutubeExplode.Models;
 
 namespace YoutubeDownloader.ViewModels.Dialogs
 {
-    public class DownloadSingleSetupViewModel : DialogScreen<DownloadViewModel>
-    {
-        private readonly IViewModelFactory _viewModelFactory;
-        private readonly SettingsService _settingsService;
-        private readonly DialogManager _dialogManager;
+	public class DownloadSingleSetupViewModel : DialogScreen<DownloadViewModel>
+	{
+		private readonly IViewModelFactory _viewModelFactory;
+		private readonly SettingsService _settingsService;
+		private readonly DialogManager _dialogManager;
 
-        public Video Video { get; set; }
+		public Video Video { get; set; }
 
-        public IReadOnlyList<DownloadOption> AvailableDownloadOptions { get; set; }
+		public IReadOnlyList<DownloadOption> AvailableDownloadOptions { get; set; }
+		
+		public DownloadOption SelectedDownloadOption { get; set; }
 
-        public DownloadOption SelectedDownloadOption { get; set; }
+		public DownloadSingleSetupViewModel(IViewModelFactory viewModelFactory, SettingsService settingsService,
+			DialogManager dialogManager)
+		{
+			_viewModelFactory = viewModelFactory;
+			_settingsService = settingsService;
+			_dialogManager = dialogManager;
+		}
 
-        public DownloadSingleSetupViewModel(IViewModelFactory viewModelFactory, SettingsService settingsService,
-            DialogManager dialogManager)
-        {
-            _viewModelFactory = viewModelFactory;
-            _settingsService = settingsService;
-            _dialogManager = dialogManager;
-        }
+		protected override void OnViewLoaded()
+		{
+			base.OnViewLoaded();
 
-        protected override void OnViewLoaded()
-        {
-            base.OnViewLoaded();
+			// Select first download option matching last used format or first non-audio-only download option
+			SelectedDownloadOption =
+				AvailableDownloadOptions.FirstOrDefault(o => o.Format == _settingsService.LastFormat) ??
+				AvailableDownloadOptions.OrderByDescending(o => !o.Label.IsNullOrWhiteSpace()).FirstOrDefault();
+		}
 
-            // Select first download option matching last used format or first non-audio-only download option
-            SelectedDownloadOption =
-                AvailableDownloadOptions.FirstOrDefault(o => o.Format == _settingsService.LastFormat) ??
-                AvailableDownloadOptions.OrderByDescending(o => !o.Label.IsNullOrWhiteSpace()).FirstOrDefault();
-        }
+		public bool CanConfirm => Video != null;
 
-        public bool CanConfirm => Video != null;
+		public void Confirm()
+		{
+			var format = SelectedDownloadOption.Format;
 
-        public void Confirm()
-        {
-            var format = SelectedDownloadOption.Format;
+			// Prompt user for output file path
+			var filter = $"{format.ToUpperInvariant()} file|*.{format}";
+			var defaultFileName = FileNameGenerator.GenerateFileName(_settingsService.FileNameTemplate, Video, format);
+			var filePath = _dialogManager.PromptSaveFilePath(filter, defaultFileName);
 
-            // Prompt user for output file path
-            var filter = $"{format.ToUpperInvariant()} file|*.{format}";
-            var defaultFileName = FileNameGenerator.GenerateFileName(_settingsService.FileNameTemplate, Video, format);
-            var filePath = _dialogManager.PromptSaveFilePath(filter, defaultFileName);
+			// If canceled - return
+			if (filePath.IsNullOrWhiteSpace())
+				return;
 
-            // If canceled - return
-            if (filePath.IsNullOrWhiteSpace())
-                return;
+			// Save last used format
+			_settingsService.LastFormat = format;
 
-            // Save last used format
-            _settingsService.LastFormat = format;
+			// Create download view model
+			var download = _viewModelFactory.CreateDownloadViewModel(Video, filePath, format, SelectedDownloadOption);
+			
+			//single download must be overwritten checked
+			download.SkipExisting = true;
+			
+			// Create empty file to "lock in" the file path
+			FileEx.CreateDirectoriesForFile(filePath);
+			FileEx.CreateEmptyFile(filePath);
 
-            // Create download view model
-            var download = _viewModelFactory.CreateDownloadViewModel(Video, filePath, format, SelectedDownloadOption);
+			// Close dialog
+			Close(download);
+		}
 
-            // Create empty file to "lock in" the file path
-            FileEx.CreateDirectoriesForFile(filePath);
-            FileEx.CreateEmptyFile(filePath);
-
-            // Close dialog
-            Close(download);
-        }
-
-        public void CopyTitle() => Clipboard.SetText(DisplayName);
-    }
+		public void CopyTitle() => Clipboard.SetText(DisplayName);
+	}
 }
