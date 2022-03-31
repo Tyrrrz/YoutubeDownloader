96c29a12c7cab0905a3ce1dd425bce9b1256df48
Oleksii Holub <1935960+Tyrrrz@users.noreply.github.com>
2022-04-01 00:13:42 +0300

    wip

--- YoutubeDownloader/ViewModels/Dialogs/MessageBoxViewModel.cs
+++ YoutubeDownloader/ViewModels/Dialogs/MessageBoxViewModel.cs
@@ -7,20 +7,41 @@ public class MessageBoxViewModel : DialogScreen
     public string? Title { get; set; }
 
     public string? Message { get; set; }
+
+    public bool IsOkButtonVisible { get; set; } = true;
+
+    public string? OkButtonText { get; set; }
+
+    public bool IsCancelButtonVisible { get; set; }
+
+    public string? CancelButtonText { get; set; }
+
+    public int ButtonsCount =>
+        (IsOkButtonVisible ? 1 : 0) +
+        (IsCancelButtonVisible ? 1 : 0);
 }
 
 public static class MessageBoxViewModelExtensions
 {
     public static MessageBoxViewModel CreateMessageBoxViewModel(
         this IViewModelFactory factory,
-        string title,
-        string message)
+        string title, string message,
+        string? okButtonText, string? cancelButtonText)
     {
         var viewModel = factory.CreateMessageBoxViewModel();
-
         viewModel.Title = title;
         viewModel.Message = message;
 
+        viewModel.IsOkButtonVisible = !string.IsNullOrWhiteSpace(okButtonText);
+        viewModel.OkButtonText = okButtonText;
+        viewModel.IsCancelButtonVisible = !string.IsNullOrWhiteSpace(cancelButtonText);
+        viewModel.CancelButtonText = cancelButtonText;
+
         return viewModel;
     }
+
+    public static MessageBoxViewModel CreateMessageBoxViewModel(
+        this IViewModelFactory factory,
+        string title, string message) =>
+        factory.CreateMessageBoxViewModel(title, message, "CLOSE", null);
 }
