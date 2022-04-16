5b4280431d978f41126f466e995572eac0b0580e
Oleksii Holub <1935960+Tyrrrz@users.noreply.github.com>
2022-04-16 20:14:42 +0300

    Use the `≤` symbol in quality preference display names

--- YoutubeDownloader.Core/Downloading/VideoQualityPreference.cs
+++ YoutubeDownloader.Core/Downloading/VideoQualityPreference.cs
@@ -16,9 +16,9 @@ public static class VideoQualityPreferenceExtensions
     public static string GetDisplayName(this VideoQualityPreference preference) => preference switch
     {
         VideoQualityPreference.Lowest => "Lowest",
-        VideoQualityPreference.UpTo480p => "<= 480p",
-        VideoQualityPreference.UpTo720p => "<= 720p",
-        VideoQualityPreference.UpTo1080p => "<= 1080p",
+        VideoQualityPreference.UpTo480p => "≤ 480p",
+        VideoQualityPreference.UpTo720p => "≤ 720p",
+        VideoQualityPreference.UpTo1080p => "≤ 1080p",
         VideoQualityPreference.Highest => "Highest",
         _ => throw new ArgumentOutOfRangeException(nameof(preference))
     };
