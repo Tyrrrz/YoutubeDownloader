43054bd91362a86f1fcf5aeedbb06819e9615643
Roberto Blázquez <elroby444@gmail.com>
2023-07-01 01:33:51 +0200

    Fix typo

--- YoutubeDownloader/Views/Dialogs/BrowserView.xaml.cs
+++ YoutubeDownloader/Views/Dialogs/BrowserView.xaml.cs
@@ -14,15 +14,15 @@ public partial class BrowserView
     }
     private async void WebBrowser_OnNavigationCompleted(object? sender, CoreWebView2NavigationCompletedEventArgs e)
     {
-        var cookies = await webBrowser.CoreWebView2.CookieManager.GetCookiesAsync("https://www.youtube.com");
+        var cookies = await WebBrowser.CoreWebView2.CookieManager.GetCookiesAsync("https://www.youtube.com");
         var isLogged = cookies?.Any(i => i.Name == "__Secure-3PSID") ?? false;
         
         if (isLogged)
         {
             ViewModel.Sapisid = cookies?.FirstOrDefault(i => i.Name == "SAPISID")?.Value ?? cookies?.FirstOrDefault(i => i.Name == "__Secure-3PAPISID")?.Value;
             ViewModel.Psid = cookies?.FirstOrDefault(i => i.Name == "__Secure-3PSID")?.Value;
-            webBrowser.CoreWebView2.CookieManager.DeleteAllCookies();
-            webBrowser.Dispose();
+            WebBrowser.CoreWebView2.CookieManager.DeleteAllCookies();
+            WebBrowser.Dispose();
             ViewModel.Close(true);
         }
     }
