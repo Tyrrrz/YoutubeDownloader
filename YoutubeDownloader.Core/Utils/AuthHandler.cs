3b7c4e2a678a3a91e105667136d7a5b80a2dd482
xBaank <robertobl022@gmail.com>
2023-07-04 01:19:46 +0200

    Use CookieHandler

--- YoutubeDownloader.Core/Utils/AuthHandler.cs
+++ YoutubeDownloader.Core/Utils/AuthHandler.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Security.Cryptography;
 using System.Text;
@@ -12,32 +13,41 @@ namespace YoutubeDownloader.Core.Utils;
 
 public class AuthHandler : DelegatingHandler
 {
-    public AuthHandler() => InnerHandler = new HttpClientHandler();
-    
-    public Dictionary<string,string> Cookies { set; get; } = new();
+    private readonly CookieContainer _cookieContainer = new();
 
-    protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken) 
+    public AuthHandler() => InnerHandler = new HttpClientHandler()
     {
+        CookieContainer = _cookieContainer
+    };
+    
+    public Dictionary<string,string> Cookies
+    {
+        set => _cookieContainer.SetCookies(new Uri("https://www.youtube.com"), value.Select(i => $"{i.Key}={i.Value}").Join(","));
+        get => _cookieContainer.GetCookies(new Uri("https://www.youtube.com")).DistinctBy(i => i.Name).ToDictionary(i => i.Name, i => i.Value);
+    }
+
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken) 
+    {
+        const string origin = "https://www.youtube.com";
         var papisid = Cookies.TryGetValue("SAPISID") ?? Cookies.TryGetValue("__Secure-3PAPISID");
         
         if (papisid is null)
-            return base.SendAsync(request, cancellationToken);
-        
-        const string origin = "https://www.youtube.com";
-        
+            return await base.SendAsync(request, cancellationToken);
+
         request.Headers.Remove("Cookie");
         request.Headers.Remove("Authorization");
         request.Headers.Remove("Origin");
         request.Headers.Remove("X-Origin");
         request.Headers.Remove("Referer");
+        request.Headers.Remove("X-Goog-AuthUser");
         
-        request.Headers.Add("Cookie", Cookies.Select(i => $"{i.Key}={i.Value}").Join("; "));
         request.Headers.Add("Authorization", $"SAPISIDHASH {GenerateSidBasedAuth(papisid, origin)}");
         request.Headers.Add("Origin", origin);
         request.Headers.Add("X-Origin", origin);
         request.Headers.Add("Referer", origin);
         
-        return base.SendAsync(request, cancellationToken);
+        var response = await base.SendAsync(request, cancellationToken);
+        return response;
     }
     
     private static string GenerateSidBasedAuth(string sid, string origin)
