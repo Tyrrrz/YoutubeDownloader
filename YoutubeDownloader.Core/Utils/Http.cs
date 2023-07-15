2f46a1a7daac13c93e5d97f1d418911f4e73f4a3
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2023-07-16 01:07:20 +0300

    Re-add user agent header to HTTP client

--- YoutubeDownloader.Core/Utils/Http.cs
+++ YoutubeDownloader.Core/Utils/Http.cs
@@ -1,8 +1,23 @@
 ﻿using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Reflection;
 
 namespace YoutubeDownloader.Core.Utils;
 
 public static class Http
 {
-    public static HttpClient Client { get; } = new();
+    public static HttpClient Client { get; } = new()
+    {
+        DefaultRequestHeaders =
+        {
+            // Required by some of the services we're using
+            UserAgent =
+            {
+                new ProductInfoHeaderValue(
+                    "YoutubeDownloader",
+                    Assembly.GetExecutingAssembly().GetName().Version?.ToString(3)
+                )
+            }
+        }
+    };
 }
