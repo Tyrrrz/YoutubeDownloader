f45cdc7697289e9bd4c62bd65d6dc579add1543b
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2023-11-15 14:50:28 +0200

    Update FFmpeg

--- YoutubeDownloader/DownloadFFmpeg.ps1
+++ YoutubeDownloader/DownloadFFmpeg.ps1
@@ -13,7 +13,7 @@ Write-Host "Downloading FFmpeg..."
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $http = New-Object System.Net.WebClient
 try {
-    $http.DownloadFile("https://github.com/Tyrrrz/FFmpegBin/releases/download/6.0/ffmpeg-windows-x64.zip", "$ffmpegFilePath.zip")
+    $http.DownloadFile("https://github.com/Tyrrrz/FFmpegBin/releases/download/6.1/ffmpeg-windows-x64.zip", "$ffmpegFilePath.zip")
 } finally {
     $http.Dispose()
 }
@@ -21,7 +21,7 @@ try {
 try {
     Import-Module "$PSHOME/Modules/Microsoft.PowerShell.Utility" -Function Get-FileHash
     $hashResult = Get-FileHash "$ffmpegFilePath.zip" -Algorithm SHA256
-    if ($hashResult.Hash -ne "29289b1008a8fadbb012e7dc0e325fea9eebbe87ac2019a4fa7df7fc15af02d0") {
+    if ($hashResult.Hash -ne "48130a80aebffb61d06913350c3ad3187efd85096f898045fd65001bf89d7d7f") {
         throw "Failed to verify the hash of the FFmpeg archive."
     }
 
