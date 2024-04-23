bf50871b62b12f482cf957781237851606e3546f
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2024-04-23 21:02:59 +0300

    Improve styling

--- YoutubeDownloader.Core/Downloading/VideoQualityPreference.cs
+++ YoutubeDownloader.Core/Downloading/VideoQualityPreference.cs
@@ -2,30 +2,29 @@
 
 namespace YoutubeDownloader.Core.Downloading;
 
-// ReSharper disable InconsistentNaming
 public enum VideoQualityPreference
 {
+    // ReSharper disable InconsistentNaming
     Lowest,
     UpTo360p,
     UpTo480p,
     UpTo720p,
     UpTo1080p,
     Highest
+    // ReSharper restore InconsistentNaming
 }
-
-// ReSharper restore InconsistentNaming
 
 public static class VideoQualityPreferenceExtensions
 {
     public static string GetDisplayName(this VideoQualityPreference preference) =>
         preference switch
         {
-            VideoQualityPreference.Lowest => "Lowest",
+            VideoQualityPreference.Lowest => "Lowest quality",
             VideoQualityPreference.UpTo360p => "≤ 360p",
             VideoQualityPreference.UpTo480p => "≤ 480p",
             VideoQualityPreference.UpTo720p => "≤ 720p",
             VideoQualityPreference.UpTo1080p => "≤ 1080p",
-            VideoQualityPreference.Highest => "Highest",
+            VideoQualityPreference.Highest => "Highest quality",
             _ => throw new ArgumentOutOfRangeException(nameof(preference))
         };
 }
