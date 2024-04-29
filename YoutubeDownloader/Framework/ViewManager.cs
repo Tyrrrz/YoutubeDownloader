6b8f2b141a0737f3f67b05f6551e3f94531e656f
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2024-04-29 21:56:32 +0300

    Revert "test"
    This reverts commit 252ec28a11f4187014a869fe9ed868c3ba1da5e9.

--- YoutubeDownloader/Framework/ViewManager.cs
+++ YoutubeDownloader/Framework/ViewManager.cs
@@ -1,33 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
 using Avalonia.Controls;
 using Avalonia.Controls.Templates;
-using YoutubeDownloader.ViewModels;
-using YoutubeDownloader.ViewModels.Components;
-using YoutubeDownloader.ViewModels.Dialogs;
-using YoutubeDownloader.Views;
-using YoutubeDownloader.Views.Components;
-using YoutubeDownloader.Views.Dialogs;
 
 namespace YoutubeDownloader.Framework;
 
 public partial class ViewManager
 {
-    private static readonly IReadOnlyDictionary<Type, Type> ViewModelTypeToViewTypeMap =
-        new Dictionary<Type, Type>
-        {
-            [typeof(DashboardViewModel)] = typeof(DashboardView),
-            [typeof(AuthSetupViewModel)] = typeof(AuthSetupView),
-            [typeof(DownloadMultipleSetupViewModel)] = typeof(DownloadMultipleSetupView),
-            [typeof(DownloadSingleSetupViewModel)] = typeof(DownloadSingleSetupView),
-            [typeof(MessageBoxViewModel)] = typeof(MessageBoxView),
-            [typeof(SettingsViewModel)] = typeof(SettingsView),
-            [typeof(MainViewModel)] = typeof(MainView)
-        };
-
     public Control? TryBindView(ViewModelBase viewModel)
     {
-        if (!ViewModelTypeToViewTypeMap.TryGetValue(viewModel.GetType(), out var type))
+        var name = viewModel
+            .GetType()
+            .FullName?.Replace("ViewModel", "View", StringComparison.Ordinal);
+
+        if (string.IsNullOrWhiteSpace(name))
+            return null;
+
+        var type = Type.GetType(name);
+        if (type is null)
             return null;
 
         if (Activator.CreateInstance(type) is not Control view)
