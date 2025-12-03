68e9b079ba169c3d43d7c1fa87780fdb1b4289a4
Tyrrrz <1935960+Tyrrrz@users.noreply.github.com>
2025-12-03 23:47:53 +0200

    Clean up

--- YoutubeDownloader.Core/Downloading/VideoQualityPreference.cs
+++ YoutubeDownloader.Core/Downloading/VideoQualityPreference.cs
@@ -16,15 +16,18 @@ public enum VideoQualityPreference
 
 public static class VideoQualityPreferenceExtensions
 {
-    public static string GetDisplayName(this VideoQualityPreference preference) =>
-        preference switch
-        {
-            VideoQualityPreference.Lowest => "Lowest quality",
-            VideoQualityPreference.UpTo360p => "≤ 360p",
-            VideoQualityPreference.UpTo480p => "≤ 480p",
-            VideoQualityPreference.UpTo720p => "≤ 720p",
-            VideoQualityPreference.UpTo1080p => "≤ 1080p",
-            VideoQualityPreference.Highest => "Highest quality",
-            _ => throw new ArgumentOutOfRangeException(nameof(preference)),
-        };
+    extension(VideoQualityPreference preference)
+    {
+        public string GetDisplayName() =>
+            preference switch
+            {
+                VideoQualityPreference.Lowest => "Lowest quality",
+                VideoQualityPreference.UpTo360p => "≤ 360p",
+                VideoQualityPreference.UpTo480p => "≤ 480p",
+                VideoQualityPreference.UpTo720p => "≤ 720p",
+                VideoQualityPreference.UpTo1080p => "≤ 1080p",
+                VideoQualityPreference.Highest => "Highest quality",
+                _ => throw new ArgumentOutOfRangeException(nameof(preference)),
+            };
+    }
 }
