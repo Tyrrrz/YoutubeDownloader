baa0e3e17d0491055515cf23f650a3e77aa229cf
Blu3Souls <Blu3SoulsIT@gmail.com>
2026-03-02 18:06:29 +0100

    implemented changes

--- YoutubeDownloader/StartOptions.cs
+++ YoutubeDownloader/StartOptions.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections;
-using System.Diagnostics.CodeAnalysis;
 using System.IO;
 
 namespace YoutubeDownloader;
@@ -14,56 +12,22 @@ public partial class StartOptions
 {
     private static readonly string SettingsFileName = "Settings.dat";
 
-    private static readonly string SettingsPathVariable = "YOUTUBEDOWNLOADER_SETTINGS_DIR";
+    private static readonly string SettingsPathVariable = "YOUTUBEDOWNLOADER_SETTINGS_PATH";
 
     private static readonly string DefaultSettingsPath = Path.Combine(
         AppContext.BaseDirectory,
         SettingsFileName
     );
 
-    public static StartOptions Current { get; } = Parse(Environment.GetEnvironmentVariables());
-
-    private static StartOptions Parse(IDictionary environmentVars) =>
-        new() { SettingsPath = GetSettingsPath(environmentVars) };
-
-    private static string GetSettingsPath(IDictionary environmentVars)
-    {
-        if (!environmentVars.Contains(SettingsPathVariable))
-            return DefaultSettingsPath;
-
-        var pathFromEnv = environmentVars[SettingsPathVariable] as string;
-        if (string.IsNullOrWhiteSpace(pathFromEnv))
-            return DefaultSettingsPath;
-
-        // Check Environment Variables first.
-        if (TryMakeValidPath(pathFromEnv, out var result))
-            return result;
-
-        // Fall back to default path.
-        return DefaultSettingsPath;
-    }
-
-    private static bool TryMakeValidPath(
-        string pathToDirectory,
-        [NotNullWhen(true)] out string? result
-    )
-    {
-        try
+    public static StartOptions Current { get; } =
+        new()
         {
-            // Normalize the directory path and ensure it is a valid filesystem path.
-            var fullDirectoryPath = Path.GetFullPath(pathToDirectory);
-
-            // Combine with the settings file name and normalize the full file path.
-            var pathToFile = Path.Combine(fullDirectoryPath, SettingsFileName);
-            var fullFilePath = Path.GetFullPath(pathToFile);
-            result = fullFilePath;
-            return true;
-        }
-        catch (Exception)
-        {
-            // Any exception indicates an invalid path.
-            result = null;
-            return false;
-        }
-    }
+            SettingsPath = Environment.GetEnvironmentVariable(SettingsPathVariable)
+                is { } path
+                    and not ""
+                ? Path.EndsInDirectorySeparator(path) || Directory.Exists(path)
+                    ? Path.Combine(path, SettingsFileName)
+                    : path
+                : DefaultSettingsPath,
+        };
 }
