3e487073e70177d69f5e62acc524d48256106b87
Oleksii Holub <1935960+Tyrrrz@users.noreply.github.com>
2026-04-14 12:18:52 +0300

    Update Language.cs

--- YoutubeDownloader/Localization/Language.cs
+++ YoutubeDownloader/Localization/Language.cs
@@ -4,24 +4,11 @@ namespace YoutubeDownloader.Localization;
 
 public enum Language
 {
-    [Description("System Default")]
     System,
-
-    [Description("Chinese (Simplified)")]
-    ChineseSimplified,
-
-    [Description("English")]
     English,
-
-    [Description("Français")]
-    French,
-
-    [Description("Deutsch")]
-    German,
-
-    [Description("Español")]
-    Spanish,
-
-    [Description("Українська")]
     Ukrainian,
+    German,
+    French,
+    Spanish,
+    ChineseSimplified,
 }
